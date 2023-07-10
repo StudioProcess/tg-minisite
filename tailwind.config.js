26dbd94ac04c2f096e2180f48adfb3f020a5ecbd
Martin Grödl <martin@process.studio>
2023-07-10 19:46:49 +0200

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./web/**/*.{html,js}"],
+  theme: {
+    fontFamily: {
+      sans: ['GT America', 'sans-serif']
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
